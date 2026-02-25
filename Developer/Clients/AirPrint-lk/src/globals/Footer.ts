283dd684b988654ab4b31c25ceddb8bb09730845
Tharuka Soooriyaarachchi <tharukaravisara@gmail.com>
2026-02-25 13:06:07 +0530

    Integrated SF UI Display fonts and implemented the new brand-aligned blue footer

--- Developer/Clients/AirPrint-lk/src/globals/Footer.ts
+++ Developer/Clients/AirPrint-lk/src/globals/Footer.ts
@@ -16,14 +16,66 @@ export const Footer: GlobalConfig = {
   },
   fields: [
     {
-      name: 'navItems',
+      name: 'tagline',
+      type: 'textarea',
+      defaultValue: 'Create the perfect packaging solution for your product!',
+    },
+    {
+      name: 'newsletterLabel',
+      type: 'text',
+      defaultValue: 'Sign up for exclusive offers and updates!',
+    },
+    {
+      name: 'columnGroups',
       type: 'array',
+      label: 'Footer Columns',
       fields: [
-        link({
-          appearances: false,
-        }),
+        {
+          name: 'title',
+          type: 'text',
+          label: 'Column Title',
+          required: true,
+        },
+        {
+          name: 'links',
+          type: 'array',
+          fields: [
+            link({
+              appearances: false,
+            }),
+          ],
+        },
       ],
-      maxRows: 6,
+      maxRows: 4,
+    },
+    {
+      name: 'socialLinks',
+      type: 'array',
+      label: 'Social Media Links',
+      fields: [
+        {
+          name: 'platform',
+          type: 'select',
+          options: [
+            { label: 'Facebook', value: 'facebook' },
+            { label: 'Instagram', value: 'instagram' },
+            { label: 'LinkedIn', value: 'linkedin' },
+            { label: 'Twitter', value: 'twitter' },
+          ],
+          required: true,
+        },
+        {
+          name: 'url',
+          type: 'text',
+          label: 'Link URL',
+          required: true,
+        },
+      ],
+    },
+    {
+      name: 'copyright',
+      type: 'text',
+      defaultValue: '© 2026 AirPrint. All Rights Reserved',
     },
   ],
 }
